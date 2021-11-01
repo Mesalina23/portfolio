00811ea8d5bea88f75e78243502ddf03dd55061f
Mesalina23 <mebrapu23@gmail.com>
2021-11-01 10:03:18 +0100

    create header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
 import '../styles/App.scss';
-
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
-      <h1>Hola mundo</h1>
+      <header>
+        <nav class='header__nav'>
+          <a href='cualquiera' title='Home'>
+            Home
+          </a>
+          <a href='cualquiera' title='About'>
+            About
+          </a>
+          <a href='cualquiera' title='Portfolio'>
+            Porfolio
+          </a>
+          <a href='cualquiera' title='Contact Me'>
+            Contact Me
+          </a>
+        </nav>
+      </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
